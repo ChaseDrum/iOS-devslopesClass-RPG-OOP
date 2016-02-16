b446b8b35c14770fabc8ed3e7c750fd23ce895c1
Chase Bank <ChaseDrum@chases-macbook-pro.local>
2016-02-16 14:03:50 -0800

    Added Character, Player and Enemy classes.

---
+++ RPG-OOP/Enemy.swift
@@ -0,0 +1,13 @@
+//
+//  Enemy.swift
+//  RPG-OOP
+//
+//  Created by Chase Drum on 2/16/16.
+//  Copyright © 2016 Chase Drum. All rights reserved.
+//
+
+import Foundation
+
+class Enemy {
+    
+}
